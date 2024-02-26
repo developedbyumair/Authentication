5c2b66774f9ec6ce1495ef52e30d65e72fc83f06
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-26 23:56:13 +0500

    Update cats.module.ts

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
+// Now any module that imports the CatsModule has access to the CatsService and will share the same instance with all other modules that import it as well.
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
+  exports: [CatsService],
 })
 export class CatsModule {}
