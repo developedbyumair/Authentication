61dba5bae1a64eb73da30c02f0b9f9acdd0c1cc7
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-26 23:59:59 +0500

    Update cats.module.ts

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
 // Now any module that imports the CatsModule has access to the CatsService and will share the same instance with all other modules that import it as well.
+
+@Global()
+//The @Global() decorator makes the module global-scoped. Global modules should be registered only once, generally by the root or core module
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
