8744d83f1ae5697330c085e392205d97c2bcf2e1
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-27 22:15:08 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
@@ -11,6 +16,9 @@ import { LoggerMiddleware } from './common/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer
+      .apply(LoggerMiddleware)
+      //n the example below, notice that we import the RequestMethod enum to reference the desired request method type.
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
   }
 }
